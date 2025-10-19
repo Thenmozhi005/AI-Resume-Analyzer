e7305a98b8105df7d1ba6e62d8cf6cc595de3ff2
Thenmozhi005 <tthenmozhij@gmail.com>
2025-10-19 22:40:45 +0530

    Fixed spaCy model loading for Streamlit

--- analyser.py
+++ analyser.py
@@ -3,7 +3,14 @@ import pdfplumber
 import re
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # -------------------------------------------------
 # Extract text from PDF
